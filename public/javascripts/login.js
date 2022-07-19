67a2be9e32256a9793584345e36a29f76ffab25b
AgusFerrando <104893201+AgusFerrando@users.noreply.github.com>
2022-07-19 14:33:40 +0200

    login fetch

---
+++ public/javascripts/login.js
@@ -0,0 +1,30 @@
+
+
+function submitSignUp(){
+    
+    let form = document.forms["signUp"];
+    let fd = new FormData(form);
+    let data = {};
+    for (let [key, prop] of fd) {
+      data[key] = prop;
+    }
+    console.log(data)
+    let VALUE = JSON.stringify(data, null, 2);
+    console.log(VALUE);
+
+    const myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json');
+
+    fetch('http://localhost:3000/users/signup', {
+        method: 'POST',
+        headers: myHeaders,
+        mode: 'cors',
+        cache: 'default',
+        body: VALUE
+    })
+    .then(data => data.json())
+    .then(data =>  { console.log(data) }) 
+    .catch((err) => {
+        console.error(err);
+    })
+}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+let usersMap = new Map()
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/signup', function(req,res,next){
+  let user = req.body
+  console.log(user)
+  usersMap.set(`${user.mail}`,user)
+  res.json({userRegistered: usersMap.get(`${user.name}`),
+            mapSize: usersMap.size })
+}) 
 
 module.exports = router;
